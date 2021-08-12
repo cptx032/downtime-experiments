2132fe1aabe73992159842f8402fda1e1932e326
Willie Lawrence <cptx032@gmail.com>
2021-08-12 20:19:59 -0300

    A24 - drop not null

--- django/downtimes/models.py
+++ django/downtimes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=512)
+    name = models.CharField(max_length=512, null=True)
 
 
 class Person(models.Model):
