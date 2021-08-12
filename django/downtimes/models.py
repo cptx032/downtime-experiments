e22f1639c14c0339fbbdb5d00e0f1a1de899c56d
Willie Lawrence <cptx032@gmail.com>
2021-08-12 20:23:45 -0300

    A16 - add constraint

--- django/downtimes/models.py
+++ django/downtimes/models.py
@@ -7,3 +7,7 @@ class Tag(models.Model):
 
 class Person(models.Model):
     name = models.CharField(max_length=512)
+    age = models.PositiveIntegerField(null=True)
+
+    class Meta:
+        unique_together = ("name", "age")
