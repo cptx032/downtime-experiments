a323f383ef54a3b0e853f4ff0d1e3b3074d17b76
Willie Lawrence <cptx032@gmail.com>
2021-08-12 19:40:30 -0300

    A18 - add index

--- django/downtimes/models.py
+++ django/downtimes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=512)
+    name = models.CharField(max_length=512, db_index=True)
 
 
 class Person(models.Model):
