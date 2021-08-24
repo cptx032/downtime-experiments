8d6d739d7efaab6b51ffe708f257a37fbbed989f
Willie Lawrence <cptx032@gmail.com>
2021-08-23 22:33:05 -0300

    wip

--- postgres/migrate.py
+++ postgres/migrate.py
@@ -12,7 +12,16 @@ connection: Con = psycopg2.connect(
     "dbname='downtimes' user='postgres' host='127.0.0.1' password='master'"
 )
 with connection:
-	cursor = connection.cursor()
-	if op_code == "A18":
-		cursor.execute("CREATE INDEX ON Tag ((name));")
-		connection.commit()
+    cursor = connection.cursor()
+    # create index
+    if op_code == "A18":
+        cursor.execute("CREATE INDEX ON Tag ((name));")
+        connection.commit()
+    # add column
+    elif op_code == "A2":
+        cursor.execute("ALTER TABLE Tag ADD COLUMN new_column int default 0;")
+        connection.commit()
+    # add fk
+    elif op_code == "A12":
+        cursor.execute("ALTER TABLE Tag ADD CONSTRAINT subtag_id_fk FOREIGN KEY(subtag_id) REFERENCES Subtag(id) ON DELETE CASCADE;")
+        connection.commit()
--- postgres/prepare_db.py
+++ postgres/prepare_db.py
@@ -2,6 +2,7 @@
 
 from typing import List, Dict
 
+import random
 import psycopg2
 import sys
 from psycopg2.extensions import connection as Con
@@ -16,16 +17,39 @@ connection: Con = psycopg2.connect(
 )
 chunk_size: int = 10000
 with connection:
-	cursor = connection.cursor()
-	if op_code == "A18":
-		cursor.execute("CREATE TABLE Tag (name varchar(255))")
-		for i in range(population):
-			print("populating {}/{}".format(i + 1, population))
-			values: List[Dict[str, str]] = [
-				{"name": get_random_string()} for i in range(chunk_size)
-			]
-			cursor.executemany(
-				"INSERT INTO Tag (name) VALUES (%(name)s)", values
-			)
-			time.sleep(0.1)
-		connection.commit()
+    with connection.cursor() as cursor:
+        if op_code in ("A18", "A2"):
+            cursor.execute("CREATE TABLE Tag (name varchar(255))")
+            for i in range(population):
+                print("populating {}/{}".format(i + 1, population))
+                values: List[Dict[str, str]] = [
+                    {"name": get_random_string()} for i in range(chunk_size)
+                ]
+                cursor.executemany(
+                    "INSERT INTO Tag (name) VALUES (%(name)s)", values
+                )
+                time.sleep(0.1)
+            connection.commit()
+        #add fk
+        elif op_code == "A12":
+            cursor.execute("CREATE TABLE Subtag (name varchar(255), id serial primary key)")
+            cursor.execute("CREATE TABLE Tag (name varchar(255), subtag_id integer)")
+            cursor.executemany(
+                "INSERT INTO Subtag (name) VALUES (%(name)s)", [
+                    {"name":  get_random_string()}
+                    for i in range(3)
+                ]
+            )
+            for i in range(population):
+                print("populating {}/{}".format(i + 1, population))
+                values: List[Dict[str, str]] = [
+                    {
+                        "name": get_random_string(),
+                        "subtag_id": random.choice([1, 2, 3])
+                    }
+                    for i in range(chunk_size)
+                ]
+                cursor.executemany(
+                    "INSERT INTO Tag (name, subtag_id) VALUES (%(name)s, %(subtag_id)s)", values
+                )
+                time.sleep(0.1)
