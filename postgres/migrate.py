c2f58e9ba2aea071c4e73c0d9fb145f8fe4ead89
Willie Lawrence <cptx032@gmail.com>
2021-08-25 08:28:52 -0300

    wip

--- postgres/migrate.py
+++ postgres/migrate.py
@@ -25,3 +25,21 @@ with connection:
     elif op_code == "A12":
         cursor.execute("ALTER TABLE Tag ADD CONSTRAINT subtag_id_fk FOREIGN KEY(subtag_id) REFERENCES Subtag(id) ON DELETE CASCADE;")
         connection.commit()
+    # drop default value
+    elif op_code == "A21":
+        cursor.execute("ALTER TABLE Tag alter column name drop default;")
+        connection.commit()
+    # add table
+    elif op_code == "A1":
+        cursor.execute("CREATE TABLE Subtag (subtag_id integer, constraint fk_ foreign key(subtag_id) references Tag(id) )")
+        connection.commit()
+    # drop column
+    elif op_code == "A6":
+        cursor.execute("ALTER TABLE Tag DROP COLUMN name;")
+        connection.commit()
+    elif op_code == "A8":
+        cursor.execute("ALTER TABLE Tag ALTER COLUMN name type varchar(255);")
+        connection.commit()
+    elif op_code == "A13":
+        cursor.execute("ALTER TABLE Subtag drop constraint fk_;")
+        connection.commit()
--- postgres/prepare_db.py
+++ postgres/prepare_db.py
@@ -18,8 +18,8 @@ connection: Con = psycopg2.connect(
 chunk_size: int = 10000
 with connection:
     with connection.cursor() as cursor:
-        if op_code in ("A18", "A2"):
-            cursor.execute("CREATE TABLE Tag (name varchar(255))")
+        if op_code in ("A18", "A2", "A1", "A6", "A8"):
+            cursor.execute("CREATE TABLE Tag (name varchar(255), id serial primary key)")
             for i in range(population):
                 print("populating {}/{}".format(i + 1, population))
                 values: List[Dict[str, str]] = [
@@ -53,3 +53,30 @@ with connection:
                     "INSERT INTO Tag (name, subtag_id) VALUES (%(name)s, %(subtag_id)s)", values
                 )
                 time.sleep(0.1)
+        # drop default value
+        elif op_code == "A21":
+            cursor.execute("CREATE TABLE Tag (name int default 0)")
+            for i in range(population):
+                print("populating {}/{}".format(i + 1, population))
+                values: List[Dict[str, str]] = [
+                    {"name": random.choice(range(1000))} for i in range(chunk_size)
+                ]
+                cursor.executemany(
+                    "INSERT INTO Tag (name) VALUES (%(name)s)", values
+                )
+                time.sleep(0.1)
+            connection.commit()
+        elif op_code == "A13":
+            cursor.execute("CREATE TABLE Tag (name varchar(255), id serial primary key)")
+            connection.commit()
+            cursor.execute("CREATE TABLE Subtag (subtag_id integer, constraint fk_ foreign key(subtag_id) references Tag(id) )")
+            for i in range(population):
+                print("populating {}/{}".format(i + 1, population))
+                values: List[Dict[str, str]] = [
+                    {"name": random.choice(range(1000))} for i in range(chunk_size)
+                ]
+                cursor.executemany(
+                    "INSERT INTO Tag (name) VALUES (%(name)s)", values
+                )
+                time.sleep(0.1)
+            connection.commit()
