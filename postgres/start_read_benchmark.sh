4e45242dd2426320652975595833ba0b8f365e96
--global <cptx032@gmail.com>
2021-12-04 20:26:21 -0300

    improving start script to allow many clients at once

--- postgres/start_read_benchmark.sh
+++ postgres/start_read_benchmark.sh
@@ -3,42 +3,93 @@
 export OP_DB_CODE=$1
 export POPULATION_PARAM=$2
 export READ_TABLE_NAME=$3
+export NUM_CLIENTS=$4
 
-export READ_LOG_PATH="./read-log-$OP_DB_CODE-$POPULATION_PARAM.txt"
+# array with read clients PIDs
+export READ_CLIENT_PIDS=()
+
+if [ -z $NUM_CLIENTS ]
+then
+    export NUM_CLIENTS=1
+fi
+
+export READ_LOG_PATH="./read-log-$OP_DB_CODE-$POPULATION_PARAM"
+
+
 export PY_EXE=$(which python)
 # how many seconds we will look before and after migration
 export NORMAL_AVG_RESPONSE_WINDOW=10
 
+setup_database() {
+    echo "Setting up database"
+    sudo -u postgres psql -c "drop database downtimes;"
+    sudo -u postgres psql -c "create database downtimes;"
+}
+
 start_read_client() {
-    $PY_EXE ./read-client.py $READ_LOG_PATH $READ_TABLE_NAME &
-    export READ_CLIENT_PID=$!
-    sleep 1
+    for i in `seq $NUM_CLIENTS`
+    do
+        # if we have more than one read-client so the log is suffixed with an ID
+        export READLOG_SUFFIX=""
+        if [ $NUM_CLIENTS -gt 1 ]
+        then
+            READLOG_SUFFIX="-${i}"
+        fi
+        $PY_EXE ./read-client.py "${READ_LOG_PATH}${READLOG_SUFFIX}.txt" $READ_TABLE_NAME &
+        READ_CLIENT_PIDS+=($!)
+        sleep 1
+    done
+    echo Read Client Started!
 }
 stop_read_client() {
-    kill -9 $READ_CLIENT_PID
+    for client_pid in "${READ_CLIENT_PIDS[@]}"
+    do
+        kill -9 $client_pid
+    done
+    echo Read Client Stopped!
+}
+
+create_empty_log_files() {
+    echo Cleaning log file
+    for i in `seq $NUM_CLIENTS`
+    do
+        export READLOG_SUFFIX=""
+        if [ $NUM_CLIENTS -gt 1 ]
+        then
+            READLOG_SUFFIX="-${i}"
+        fi
+        echo "" > "${READ_LOG_PATH}${READLOG_SUFFIX}.txt"
+    done
+}
+
+register_migration() {
+    echo $1 $(date --iso-8601=ns)
+    for i in `seq $NUM_CLIENTS`
+    do
+        export READLOG_SUFFIX=""
+        if [ $NUM_CLIENTS -gt 1 ]
+        then
+            READLOG_SUFFIX="-${i}"
+        fi
+        echo $1 $(date --iso-8601=ns) >> "${READ_LOG_PATH}${READLOG_SUFFIX}.txt"
+    done
 }
 
 
-echo Cleaning log file
-echo "" > $READ_LOG_PATH
-echo "Setting up database"
-sudo -u postgres psql -c "drop database downtimes;"
-sudo -u postgres psql -c "create database downtimes;"
+setup_database
 
 echo "Initial Migration"
 $PY_EXE ./prepare_db.py $OP_DB_CODE $POPULATION_PARAM
 
-# after the checkout the application is still running the old version
+create_empty_log_files
 start_read_client
-echo Read Client Started!
 # while this script is stopped because this sleep the read-application is
 # still running in the background
 sleep $NORMAL_AVG_RESPONSE_WINDOW
-echo "START MIGRATION" $(date --iso-8601=ns)
-echo "START MIGRATION" $(date --iso-8601=ns) >> $READ_LOG_PATH
+
+register_migration "START MIGRATION"
 $PY_EXE ./migrate.py $OP_DB_CODE
-echo "END MIGRATION" $(date --iso-8601=ns)
-echo "END MIGRATION" $(date --iso-8601=ns) >> $READ_LOG_PATH
+register_migration "END MIGRATION"
 
 echo "Getting metrics after migration"
 sleep $NORMAL_AVG_RESPONSE_WINDOW
--- postgres/start_write_benchmark.sh
+++ postgres/start_write_benchmark.sh
@@ -3,42 +3,93 @@
 export OP_DB_CODE=$1
 export POPULATION_PARAM=$2
 export WRITE_TABLE_NAME=$3
+export NUM_CLIENTS=$4
 
-export WRITE_LOG_PATH="./write-log-$OP_DB_CODE-$POPULATION_PARAM.txt"
+# array with read clients PIDs
+export WRITE_CLIENT_PIDS=()
+
+if [ -z $NUM_CLIENTS ]
+then
+    export NUM_CLIENTS=1
+fi
+
+export WRITE_LOG_PATH="./write-log-$OP_DB_CODE-$POPULATION_PARAM"
+
 export PY_EXE=$(which python)
 # how many seconds we will look before and after migration
 export NORMAL_AVG_RESPONSE_WINDOW=10
 
+setup_database() {
+    echo "Setting up database"
+    sudo -u postgres psql -c "drop database downtimes;"
+    sudo -u postgres psql -c "create database downtimes;"
+}
+
 start_write_client() {
-	$PY_EXE ./write-client.py $WRITE_LOG_PATH $WRITE_TABLE_NAME name $POPULATION_PARAM &
-    export WRITE_CLIENT_PID=$!
+    for i in `seq $NUM_CLIENTS`
+    do
+        # if we have more than one write-client so the log is suffixed with an ID
+        export WRITELOG_SUFFIX=""
+        if [ $NUM_CLIENTS -gt 1 ]
+        then
+            WRITELOG_SUFFIX="-${i}"
+        fi
+        $PY_EXE ./write-client.py "${WRITE_LOG_PATH}${WRITELOG_SUFFIX}.txt" $WRITE_TABLE_NAME name $POPULATION_PARAM &
+        WRITE_CLIENT_PIDS+=($!)
+    done
     sleep 1
+    echo Write Client Started!
 }
+
 stop_write_client() {
-	kill -9 $WRITE_CLIENT_PID
+    for client_pid in "${WRITE_CLIENT_PIDS[@]}"
+    do
+        kill -9 $client_pid
+    done
+    echo Write Client Stopped!
 }
 
 
-echo Cleaning log file
-echo "" > $WRITE_LOG_PATH
-echo "Setting up database"
-sudo -u postgres psql -c "drop database downtimes;"
-sudo -u postgres psql -c "create database downtimes;"
+create_empty_log_files() {
+    echo Cleaning log file
+    for i in `seq $NUM_CLIENTS`
+    do
+        export WRITELOG_SUFFIX=""
+        if [ $NUM_CLIENTS -gt 1 ]
+        then
+            WRITELOG_SUFFIX="-${i}"
+        fi
+        echo "" > "${WRITE_LOG_PATH}${WRITELOG_SUFFIX}.txt"
+    done
+}
+
+
+register_migration() {
+    echo $1 $(date --iso-8601=ns)
+    for i in `seq $NUM_CLIENTS`
+    do
+        export WRITELOG_SUFFIX=""
+        if [ $NUM_CLIENTS -gt 1 ]
+        then
+            WRITELOG_SUFFIX="-${i}"
+        fi
+        echo $1 $(date --iso-8601=ns) >> "${WRITE_LOG_PATH}${WRITELOG_SUFFIX}.txt"
+    done
+}
+
+setup_database
 
 echo "Initial Migration"
 $PY_EXE ./prepare_db.py $OP_DB_CODE $POPULATION_PARAM
 
-# after the checkout the application is still running the old version
+create_empty_log_files
 start_write_client
-echo Write Client Started!
 # while this script is stopped because this sleep the write-application is
 # still running in the background
 sleep $NORMAL_AVG_RESPONSE_WINDOW
-echo "START MIGRATION" $(date --iso-8601=ns)
-echo "START MIGRATION" $(date --iso-8601=ns) >> $WRITE_LOG_PATH
+register_migration "START MIGRATION"
 $PY_EXE ./migrate.py $OP_DB_CODE
-echo "END MIGRATION" $(date --iso-8601=ns)
-echo "END MIGRATION" $(date --iso-8601=ns) >> $WRITE_LOG_PATH
+register_migration "END MIGRATION"
 
 echo "Getting metrics after migration"
 sleep $NORMAL_AVG_RESPONSE_WINDOW
